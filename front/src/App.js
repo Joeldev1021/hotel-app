739d32adae8954147bdc78ecc22e557c166f8adf
unknown <joelpimentel1995@gmail.com>
2022-01-15 14:57:09 -0500

    filter by price and category

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import CardHotel from "./components/cardHotel/CardHotel";
+import Header from "./components/header/Header";
+
 
 function App() {
+  const [hotel, setHotel] = useState([]);
+
+  useEffect(() => {
+    getHotel()
+     .then(res => setHotel(res))
+  }, []);
+
+  const getHotel = async() => {
+    const resData = await fetch("http://localhost:4000/hotel");
+    const data = resData.json();
+    return data;
+  };
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Header/>
+      {
+        hotel.map(h => <CardHotel key={h.hotel_id} {...h}/>)
+      }
     </div>
   );
 }
